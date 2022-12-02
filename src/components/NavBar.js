e2827c741dd9533fbf5e8f051f3c730b0505ed58
cristianneda <cristian.neda@gmail.com>
2022-12-02 21:43:50 +0200

    latest deployment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,23 +5,23 @@ const NavBar = () => {
   return (
     <nav class="relative w-full  bg-gray-900 bg-opacity-80 text-sky-500 hover:text-sky-700 focus:text-sky-700 drop-shadow lg  ">
       <div class=" flex flex-row justify-between ">
-      <div class="">
-        <img
-          class="rounded-lg hover:scale-125 w-24 p-2"
-          src="https://media-exp1.licdn.com/dms/image/C4E03AQEMK24AfqWmnw/profile-displayphoto-shrink_800_800/0/1651284928395?e=1657756800&v=beta&t=VZpp3KybiYtvidK6acfz2FduYAXvqK3fiy22BLltsF8"
-          alt=""
-          onClick={() =>
-            window.open(
-              "https://media-exp1.licdn.com/dms/image/C4E03AQEMK24AfqWmnw/profile-displayphoto-shrink_800_800/0/1651284928395?e=1657756800&v=beta&t=VZpp3KybiYtvidK6acfz2FduYAXvqK3fiy22BLltsF8",
-              "_blank"
-            )
-          }
-        ></img>
-      </div>
-        <div class=" w-full flex p-2 self-center justify-end ">
-          <ProjectsButton  />
+        <div class="">
+          <img
+            class="rounded-lg hover:scale-125 w-24 p-2"
+            src="/public/portret.jpg"
+            alt=""
+            onClick={() =>
+              window.open(
+                "https://media-exp1.licdn.com/dms/image/C4E03AQEMK24AfqWmnw/profile-displayphoto-shrink_800_800/0/1651284928395?e=1657756800&v=beta&t=VZpp3KybiYtvidK6acfz2FduYAXvqK3fiy22BLltsF8",
+                "_blank"
+              )
+            }
+          ></img>
         </div>
-        
+        <div class=" w-full flex p-2 self-center justify-end ">
+          <ProjectsButton />
+        </div>
+
       </div>
     </nav>
   );
