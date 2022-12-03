d7cc853338e456c19c2773a38a57a33a691c9256
cristianneda <cristian.neda@gmail.com>
2022-12-03 15:33:13 +0200

    navbar update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,22 +4,26 @@ import React from "react";
 const NavBar = () => {
   return (
     <nav class="relative w-full  bg-gray-900 bg-opacity-80  text-sky-500   ">
-      <div class=" flex flex-row content-center justify-between p-2 ">
-        <img
-          class="rounded-lg max-h-28 hover:scale-125  p-2"
-          src="https://neda-node-weather-app.herokuapp.com/img/portret.jpg"
-          alt=""
-          onClick={() =>
-            window.open(
-              "https://neda-node-weather-app.herokuapp.com/img/portret.jpg",
-              "_blank"
-            )
-          }
-        ></img>
-        <a class=" hover:text-white " href="https://neda-node-weather-app.herokuapp.com/">Weather</a>
-        <a class=" hover:text-white " href="https://cristianneda.github.io/subscriber-gitlab/">Subscriber</a>
-        <a class=" hover:text-white " href="https://cristianneda.github.io/Simple-Background-Generator/">Bg-Generator</a>
-        <a class=" hover:text-white " href="https://cristianneda.github.io/redux/">Todo</a>
+      <div class=" flex flex-row  " >
+        <div>
+          <img
+            class="rounded-lg max-h-16 hover:scale-125  p-2"
+            src="https://neda-node-weather-app.herokuapp.com/img/portret.jpg"
+            alt=""
+            onClick={() =>
+              window.open(
+                "https://neda-node-weather-app.herokuapp.com/img/portret.jpg",
+                "_blank"
+              )
+            }
+          ></img>
+        </div>
+        <div class="flex auto-rows-auto gap-2 text-xs py-6 " >
+          <a class=" hover:text-white" href="https://neda-node-weather-app.herokuapp.com/">Weather</a>
+          <a class=" hover:text-white" href="https://cristianneda.github.io/subscriber-gitlab/">Subscriber</a>
+          <a class=" hover:text-white" href="https://cristianneda.github.io/Simple-Background-Generator/">Bg-Generator</a>
+          <a class=" hover:text-white" href="https://cristianneda.github.io/redux/">Todo</a>
+        </div>
       </div>
     </nav>
   );
